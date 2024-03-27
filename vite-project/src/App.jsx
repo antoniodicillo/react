f33af95e7d5b0540549d247cb01504dabb20d645
Tiago Marchetti Lyra <henriquesilvasanches@gmail.com>
2024-03-27 09:28:31 -0300

    primeiro react

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import './Componentes/Banner'
+import Banner from './Componentes/Banner'
+
+function App() {
+  return (
+    <>
+      <Banner></Banner>
+    </>
+  )
+}
+
+export default App
---
+++ vite-project/src/Componentes/Banner.jsx
@@ -0,0 +1,10 @@
+function Banner() {
+    return(
+        <> 
+            <h1>Banner</h1>
+            <p>Esse é meu banner</p>
+        </> 
+    )
+}
+
+export default Banner
