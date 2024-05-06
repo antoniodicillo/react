1f66754811d4c7dba608a96c77adf96e7923656b
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-06 11:32:12 -0300

    aula

---
+++ React_2Tri/src/pages/Filmes.jsx
@@ -0,0 +1,9 @@
+function Filmes() {
+    return ( 
+        <>
+            <h1>Filmes</h1>
+        </>
+     );
+}
+
+export default Filmes;
---
+++ React_2Tri/src/pages/PageNotFound.jsx
@@ -0,0 +1,9 @@
+function PageNotFound() {
+    return (
+        <>
+            <h1>Error: 404</h1>
+        </>
+    )
+}
+
+export default PageNotFound
