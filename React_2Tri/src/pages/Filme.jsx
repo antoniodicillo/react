833e09155c6ed3e5401a12bcb61621b228e2ed96
antoniodicillo <125458796+antoniodicillo@users.noreply.github.com>
2024-06-06 13:35:42 -0300

    d

--- React_2Tri/src/pages/Filme.jsx
+++ React_2Tri/src/pages/Filme.jsx
@@ -3,7 +3,8 @@ import { useState } from "react";
 function Filme() {
   const [filmeDetails, setFilmeDetails] = useState([]);
   let currentMovie = localStorage.getItem('Filme')
- 
+  let posterImagem = 'https://image.tmdb.org/t/p/w500/'
+  
 
     fetch("https://api.themoviedb.org/3/movie/"+currentMovie+'?api_key=7c572a9f5b3ba776080330d23bb76e1e')
     .then((response) => response.json())
@@ -12,7 +13,10 @@ function Filme() {
 
   return (
     <>      
-      <h1>{filmeDetails.title}</h1>
+      <div className="imgFilmeBackground" >  
+        <img className='imgFilmeBackground' src={posterImagem+filmeDetails.backdrop_path} alt="" />
+        <h1 className="color-white">{filmeDetails.title}</h1>
+      </div>
     </>
   );
 }
